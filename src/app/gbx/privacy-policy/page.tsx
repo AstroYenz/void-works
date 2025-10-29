24c282035ee75283b418940e3d7082c361be3c45
Jens Astrup <JensAstrup@users.noreply.github.com>
2025-10-29 17:22:30 -0400

    Add gbx privacy policy (#27)
    * Add privacy policy
    
    * Update styling

---
+++ src/app/gbx/privacy-policy/page.tsx
@@ -0,0 +1,12 @@
+import PrivacyPolicy from '@/app/gbx/privacy-policy/privacy-policy'
+
+
+function PrivacyPolicyPage(): React.ReactNode {
+  return (
+    <div className="container mx-auto px-6 sm:py-16">
+      <PrivacyPolicy />
+    </div>
+  )
+}
+
+export default PrivacyPolicyPage
